0f78cd21d43b7a1da4649779c6a5680c30c15657
Wmac23 <william@macallan.dk>
2023-05-29 15:24:15 +0200

    commit16

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,31 @@
-import React from "react"
-import StyledFirebaseAuth from "react-firebaseui"
-import firebase from "../../firebase/clientApp"
+import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "../../firebase/clientApp";
 
+// Configure FirebaseUI.
 const uiConfig = {
-
-signInSuccessUrl: "/home",
-
-signInOptions: [firebase.auth.GithubProvider.PROVIDER_ID],
-
+  // Redirect to / after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/home",
+  // We will display GitHub as auth providers.
+  signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID],
 };
 
 function SignIn() {
-  return(
-    <div>
-    <h1>PókeDex Login</h1>
-    <p>Please sign in</p>
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+  return (
+    <div
+      style={{
+        maxWidth: "320px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Pineapple Login</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
-  );  
+  );
 }
 
 export default SignIn;
