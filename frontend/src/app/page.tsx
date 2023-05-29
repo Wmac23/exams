3be026115bae58398f634754444099a4532cb3f9
Wmac23 <william@macallan.dk>
2023-05-29 15:02:54 +0200

    commit15

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import React from "react"
+import StyledFirebaseAuth from "react-firebaseui"
+import firebase from "../../firebase/clientApp"
+
+const uiConfig = {
+
+signInSuccessUrl: "/home",
+
+signInOptions: [firebase.auth.GithubProvider.PROVIDER_ID],
+
+};
+
+function SignIn() {
   return(
-    <h1>Hello</h1>
-  )
+    <div>
+    <h1>PókeDex Login</h1>
+    <p>Please sign in</p>
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+    </div>
+  );  
 }
+
+export default SignIn;
