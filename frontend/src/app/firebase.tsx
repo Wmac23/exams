63d2430c7fe8d9667c03694ba67f5859e609a33a
Wmac23 <william@macallan.dk>
2023-05-29 16:00:24 +0200

    commit18

---
+++ frontend/src/app/firebase.tsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKO9iXG4kKa6igr2IcdHBLt_jn-IRLV_k",
+  authDomain: "exam2023-8191c.firebaseapp.com",
+  projectId: "exam2023-8191c",
+  storageBucket: "exam2023-8191c.appspot.com",
+  messagingSenderId: "669644845356",
+  appId: "1:669644845356:web:c687199f8e61519e3dfc89"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { auth } from "firebase/auth"
+import {signInWithPopup,} from "firebase/auth"
+export default function Home() {
+  return (
+    <main>
+<h1>Sign in</h1>
+<button>Login</button>
+    </main>
+  )
+}
