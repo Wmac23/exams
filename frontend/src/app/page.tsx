ad1a197de370c79c3664332a4eefd4da84673088
Wmac23 <william@macallan.dk>
2023-05-30 12:45:39 +0200

    commit21

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,49 +1,19 @@
-'use client';
+import { auth } from "./firebase";
+import {signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import { async } from "@firebase/util"; 
 
-import { useEffect, useState } from "react";
-import { auth } from "../../config/firebaseAuth";
+export default function Home() {
 
-const Login = () => {
-  const [user, setUser] = useState<firebase.User | null>(null);
-
-  useEffect(() => {
-    // Listen for authentication state changes
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-
-    return () => unsubscribe(); // Cleanup function
-  }, []);
-
-  const handleLogin = async () => {
-    try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      await auth.signInWithPopup(provider);
-    } catch (error) {
-      console.error("Error logging in:", error);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      await auth.signOut();
-    } catch (error) {
-      console.error("Error logging out:", error);
-    }
-  };
-
+  const googleAuth = new GoogleAuthProvider();
+  const login = async() => {
+    const result = await signInWithPopup(auth, googleAuth);
+  };  
   return (
-    <div>
-      {user ? (
-        <>
-          <p>Welcome, {user.displayName}!</p>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <button onClick={handleLogin}>Login with Google</button>
-      )}
-    </div>
-  );
-};
-
-export default Login;
+<main>
+<div>
+<h1>Sign in</h1>
+<button onClick={login}>Login</button>
+</div>
+</main>
+  )
+}
