cd4c3dd63af53cdc029fd5ba65d78fef8e8fc3f0
Wmac23 <william@macallan.dk>
2023-12-28 14:58:52 +0100

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,13 +8,11 @@ export default function Home() {
       <Navbar />
       <div>
         <h1>Velkommen til Passagertal.dk</h1>
-        <h2>H2</h2>
-        <h3>H3</h3>
         <p>Hvor mange busrejser foretages? Hvor flyver vi hen? Og på hvilke tidspunkter er der flest, der tjekker ind med Rejsekortet?</p>
-        <button className={styles.buttons}><img src="plane.png" alt="Icon of a plane"/><br/>Luftfart </button>
-        <button className={styles.buttons}><img src="train.png" alt="Icon of a train"/><br/>Jernbaner</button>
-        <button className={styles.buttons}><img src="bus.png" alt="Icon of a bus"/><br/>Busser</button>
-        <button className={styles.buttons}><img src="card.png" alt="Icon of a card"/><br/>Rejsekort</button>
+        <a href="luftfart"><button className={styles.buttons}><img src="plane.png" alt="Icon of a plane"/><br/>Luftfart </button></a>
+        <a href="jernbaner"><button className={styles.buttons}><img src="train.png" alt="Icon of a train"/><br/>Jernbaner</button></a>
+        <a href="busser"><button className={styles.buttons}><img src="bus.png" alt="Icon of a bus"/><br/>Busser</button></a>
+        <a href="rejsekort"><button className={styles.buttons}><img src="card.png" alt="Icon of a card"/><br/>Rejsekort</button></a>
       </div>
       <Footer/>
     </main>
