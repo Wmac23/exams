d04746472f92998d4d64dfc4b4643bffe61feab3
Wmac23 <william@macallan.dk>
2024-01-08 09:13:21 +0100

    final

--- frontend/src/app/busser/page.tsx
+++ frontend/src/app/busser/page.tsx
@@ -6,8 +6,8 @@ export default function Busser() {
     <main className={styles.main}>
         <div>
           <h1 className={styles.text}>Busser</h1>
-        <button><img src="bus-station.png" alt="Icon for public busses"/><br/><h4>Offentlige busser</h4><h2>240 mio.</h2><p>Påstigere 2022</p></button>
-        <button><img src="bus-far.png" alt="Icon for busses that drives far distances"/><br/><h4>Fjernbusser</h4><h2>1,40 mio</h2><p>Påstigere 2022</p></button>
+        <button><img src="bus-station.png" alt="Icon for public busses"/><br/><p>Offentlige busser</p><h2>240 mio.</h2><p>Påstigere 2022</p></button>
+        <button><img src="bus-far.png" alt="Icon for busses that drives far distances"/><br/><p>Fjernbusser</p><h2>1,40 mio</h2><p>Påstigere 2022</p></button>
       </div>
     </main>
   )
